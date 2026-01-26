650767b6d0988d2a635cb052b7118386390c20d4
tolstosheev17 <dane4ka.06@mail.ru>
2026-01-26 17:00:49 +0300

    Add slider component and modules

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,43 @@
+ import 'swiper/css'
+import './Slider.scss'
+ import SliderNavigation from "./components/SliderNavigation";
+
+const defaultSliderParams = {
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+    spaceBetween: 30,
+}
+
+const Slider = (props) => {
+    const {
+        children,
+        navigationTargetElementId = null,
+        sliderParams = defaultSliderParams,
+    } = props
+    return (
+        <div
+            className="slider"
+            data-js-slider={JSON.stringify({
+                sliderParams,
+                navigationTargetElementId,
+            })}
+        >
+            <div className="slider__swiper swiper" data-js-slider-swiper="">
+                <ul className="slider__list swiper-wrapper">
+                    {children.map((slide, index) => (
+                        <li className="slider__item swiper-slide" key={index}>
+                            {slide}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {!navigationTargetElementId && (
+                <SliderNavigation
+                    className="slider__navigation"
+                />
+            )}
+        </div>
+    )
+}
+
+ export default Slider
--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,62 +1,26 @@
 import Section from "@/layouts/Section";
 import CategoryCard from "@/components/CategoryCard";
+import Slider from "@/components/Slider";
+import SliderNavigation from "@/components/Slider/components/SliderNavigation";
+import categoryItems from "@/sections/Categories/categoryItems";
 
 const Categories = () => {
-    const categoryItems = [
-        {
-            title: 'Action',
-            images: [
-                '/src/assets/images/categories/action/1.jpg',
-                '/src/assets/images/categories/action/2.jpg',
-                '/src/assets/images/categories/action/3.jpg',
-                '/src/assets/images/categories/action/4.jpg',
-            ]
-        },
-        {
-            title: 'Adventure',
-            images: [
-                '/src/assets/images/categories/adventure/1.jpg',
-                '/src/assets/images/categories/adventure/2.jpg',
-                '/src/assets/images/categories/adventure/3.jpg',
-                '/src/assets/images/categories/adventure/4.jpg',
-            ]
-        },
-        {
-            title: 'Comedy',
-            images: [
-                '/src/assets/images/categories/comedy/1.jpg',
-                '/src/assets/images/categories/comedy/2.jpg',
-                '/src/assets/images/categories/comedy/3.jpg',
-                '/src/assets/images/categories/comedy/4.jpg',
-            ]
-        },
-        {
-            title: 'Drama',
-            images: [
-                '/src/assets/images/categories/drama/1.jpg',
-                '/src/assets/images/categories/drama/2.jpg',
-                '/src/assets/images/categories/drama/3.jpg',
-                '/src/assets/images/categories/drama/4.jpg',
-            ]
-        },
-        {
-            title: 'Horror',
-            images: [
-                '/src/assets/images/categories/horror/1.jpg',
-                '/src/assets/images/categories/horror/2.jpg',
-                '/src/assets/images/categories/horror/3.jpg',
-                '/src/assets/images/categories/horror/4.jpg',
-            ]
-        },
-    ]
-
+    const slideNavigationId = "categories-slider-navigation"
     return (
         <Section
             title="Explore our wide variety of categories"
             titleId="categories-title"
             description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
             isActionsHiddenOnMobile
-            actions
+            actions = {(
+                <SliderNavigation
+                    mode="tile"
+                    id={slideNavigationId}
+                />
+            )}
+        >
+        <Slider
+            navigationTargetElementId={slideNavigationId}
         >
             {categoryItems.map((categoryItem, index) => (
                 <CategoryCard
@@ -64,6 +28,7 @@ const Categories = () => {
                     key={index}
                 />
             ))}
+        </Slider>
         </Section>
     )
 }
